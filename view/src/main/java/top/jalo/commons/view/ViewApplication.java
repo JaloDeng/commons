9993491331fb5317200e2c279a91d506c111ae02
Jalo <651379728@qq.com>
2018-01-17 16:54:06 +0800

    View : Delete redundant code.

--- view/src/main/java/top/jalo/commons/view/ViewApplication.java
+++ view/src/main/java/top/jalo/commons/view/ViewApplication.java
@@ -3,12 +3,10 @@ package top.jalo.commons.view;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import top.jalo.commons.security.SecurityApplication;
-
 @SpringBootApplication
 public class ViewApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(new Object[] {SecurityApplication.class, ViewApplication.class}, args);
+		SpringApplication.run(ViewApplication.class, args);
 	}
 }
